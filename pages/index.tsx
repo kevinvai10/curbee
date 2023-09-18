590643ad27a99b1b6817695beefd27229dcfbfcb
kevinvai10 <kevin.cbdev@gmail.com>
2023-09-18 20:56:01 +0200

    add redirect to log in from home

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,4 +2,13 @@ function Home() {
     return <div>Home Page</div>;
 }
 
+export const getServerSideProps = async () => {
+    return {
+        redirect: {
+            permanent: false,
+            destination: `/login`,
+        },
+    };
+};
+
 export default Home;
