204177e1ebf3ee94ad8c5687ba1d4b7e0c06433b
kevinvai10 <kevin.cbdev@gmail.com>
2023-09-25 19:06:47 +0200

    add time manipulation for card display

--- components/AppointmentCard/AppointmentCard.tsx
+++ components/AppointmentCard/AppointmentCard.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
 
 import styles from './AppointmentCard.module.css';
+import { Appointment } from '../Appointments/types';
 
-const AppointmentCard = () => {
+type Props = {
+    appointment: Appointment;
+};
+
+const AppointmentCard = ({ appointment }: Props) => {
+    /**
+     * TODO: use a library that solves dates since
+     * manipulating dates with vainilla js might
+     * not consider things like daylight savings time etc
+     * */
+    const scheduledTime = new Date(appointment.scheduledTime);
+    const formattedScheduledTime = scheduledTime.toLocaleString('en-us', {
+        month: 'long',
+        day: '2-digit',
+        year: 'numeric',
+    });
+    const startTime = scheduledTime.toLocaleString('en-us', {
+        hour: 'numeric',
+    });
+    /** Assuming appointment duration is in minutes */
+    const endTimeMs =
+        scheduledTime.getTime() + appointment.duration * 60 * 1000;
+    const endTimeDate = new Date(endTimeMs).toLocaleString('en-us', {
+        hour: 'numeric',
+    });
+
     return (
         <div className={styles.card}>
             <div className={styles['card__title']}>
-                <p>March 29, 2023</p>
+                <p>{formattedScheduledTime}</p>
             </div>
             <div className={styles['card__content']}>
                 <div className={styles['card__content__date']}>
-                    <strong>Started/Completed</strong>
-                    <p>10:00 - 12:00 PM</p>
+                    <strong>{appointment.status}</strong>
+                    <p>
+                        {startTime}
+                        &nbsp;
+                        <span> - </span>
+                        &nbsp;
+                        <span>{endTimeDate}</span>
+                    </p>
                 </div>
                 <div className={styles['card__content__summary']}>
                     <p>Service</p>
                     <ul>
-                        <li>Signature</li>
-                        <li>Tire Pressure</li>
-                        <li>Eco Friendly Wash</li>
+                        <li>{appointment.workOrder.service}</li>
                     </ul>
                 </div>
             </div>
